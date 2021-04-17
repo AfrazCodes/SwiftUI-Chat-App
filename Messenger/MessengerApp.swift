d646699a3bc4a86016d8223580cd9ab41b581e92
Afraz Siddiqui <afsiddiq@microsoft.com>
2021-04-17 09:38:19 -0400

    Initial Commit

---
+++ Messenger/MessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerApp.swift
+//  Messenger
+//
+//  Created by Afraz Siddiqui on 4/17/21.
+//
+
+import SwiftUI
+
+@main
+struct MessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
